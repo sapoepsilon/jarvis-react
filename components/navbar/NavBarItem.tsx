c9ed091cb330ac001a79b65ba63f696bfb6bac44
Iana Sanduta <iana.sandutsa@gmail.com>
2023-12-19 10:43:58 -0700

    feat: Added Animation

--- components/navbar/NavBarItem.tsx
+++ components/navbar/NavBarItem.tsx
@@ -8,13 +8,18 @@ type NavbarItemProps = {
 
 const NavbarItem = ({ href, title, isActive }: NavbarItemProps) => {
   const router = useRouter();
+<<<<<<< HEAD
 
   const scrollToSection = (sectionId: string) => {
+=======
+  const scrollToSection = (sectionId: any) => {
+>>>>>>> 08c3461 (feat: Added Animation)
     const section = document.getElementById(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: "smooth" });
     }
   };
+<<<<<<< HEAD
 
   return (
       <a
@@ -31,6 +36,29 @@ const NavbarItem = ({ href, title, isActive }: NavbarItemProps) => {
       >
         {title}
       </a>
+=======
+  return (
+    <a
+      href={`#${title.toLowerCase()}`}
+      className={`px-3 py-2 rounded-md text-md font-medium ${
+        isActive
+          ? "text-white text-lg hover:text-accent-purple"
+          : "text-white hover:text-accent-purple"
+      }`}
+      onClick={(e) => {
+        e.preventDefault(); // Prevent default anchor behavior
+        scrollToSection(title.toLowerCase()); // Convert title to ID and scroll to the section
+      }}
+    >
+      {title}
+    </a>
+    // <span
+    //   onClick={() => router.push(href)}
+    //   className="py-4 px-2 text-white font-semibold hover:text-xl cursor-pointer "
+    // >
+    //   {title}
+    // </span>
+>>>>>>> 08c3461 (feat: Added Animation)
   );
 };
 
