41be28073c57aae1ab05b074c9e1229101685cc8
Ismatulla Mansurov <ismatullamansurov@gmail.com>
2024-01-11 10:50:15 -0700

    fix: completes VIU-122

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -2,12 +2,17 @@ import { FC } from 'react'
 import { supabase } from '../utils/supabaseClient'
 
 const handleGoogleLogin = async () => {
-  await supabase.auth.signInWithOAuth({ provider: 'google' })
+  
+  await supabase.auth.signInWithOAuth({
+    provider: 'google', options: {
+      redirectTo: process.env.NEXT_PUBLIC_OAUTH_REDIRECT_URI
+    }
+  })
 }
 
 const Auth: FC = () => (
   <div>
-    <button 
+    <button
       onClick={handleGoogleLogin}
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
     >
